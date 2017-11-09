4c11c3d9d89f9b255d641ee68a8c6661f5ef4ed8
Asim Sabic <asabic@domain.local>
2017-11-09 10:23:45 +0100

    Init reporsitory

---
+++ tests/figures/Test_pawn.java
@@ -0,0 +1,14 @@
+package figures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_pawn {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
